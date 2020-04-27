69ba291a388fa832f4700d4cd1d741e3dc7e3117
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-04-27 18:32:58 +0900

    Cleaned the default starting project, added MUI library, started working on the weather cards, added icons

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import sunny from "./images/sunny.png";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  weatherCard: {
+    margin: theme.spacing(1),
+    width: theme.spacing(16),
+    height: theme.spacing(16),
+  },
+  weatherIcon: {
+    height: theme.spacing(8),
+    width: theme.spacing(8),
+    alignSelf: "center",
+    justify: "center",
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={0} justify="center">
+      <Grid item>
+        <Card className={classes.weatherCard}>
+          <CardHeader
+            title="Weather 1"
+            titleTypographyProps={{ variant: "body2" }}
+            style={{ textAlign: "center" }}
+          />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <CardMedia className={classes.weatherIcon} image={sunny} />
+          </div>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card className={classes.weatherCard}>Item 2</Card>
+      </Grid>
+      <Grid item>
+        <Card className={classes.weatherCard}>Item 3</Card>
+      </Grid>
+    </Grid>
   );
 }
-
-export default App;
