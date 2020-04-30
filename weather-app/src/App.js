820da4e7953c26dc126994ac9be44a41fe06ee2b
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-04-30 20:02:13 +0900

    Small improvements in layout

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,9 +3,22 @@ import React from "react";
 import WeatherCard from "./components/WeatherCard";
 // MUI
 import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+// weather images
+import sunny from "./images/sunny.png";
+import cloud from "./images/cloud.png";
+import cloudy from "./images/cloudy.png";
+import lightning from "./images/lightning.png";
+import mooncloud from "./images/mooncloud.png";
+import moon from "./images/moon.png";
+import rain from "./images/rain.png";
+import rainbow from "./images/rainbow.png";
+import snow from "./images/snow.png";
+import sun from "./images/sun.png";
 
 const useStyles = makeStyles((theme) => ({
   forecast: {
@@ -23,13 +36,25 @@ export default function App() {
   const today = new Date();
 
   let dayCards = [];
+  let weathers = [
+    sunny,
+    cloud,
+    cloudy,
+    lightning,
+    moon,
+    mooncloud,
+    rain,
+    rainbow,
+    snow,
+    sun,
+  ];
 
   for (let i = 0; i < 8; i++) {
     const newDate = new Date(today.getTime() + i * 86400000);
 
     dayCards.push({
       date: newDate,
-      weather: i,
+      weather: weathers[i],
       maxTemp: 70 + i,
       minTemp: 70 - i,
     });
@@ -64,15 +89,44 @@ export default function App() {
               </Typography>
             </Grid>
             <Grid item xs={12}>
-              <Typography variant="subtitle1">Tuesday 5:00 PM</Typography>
+              <Typography variant="subtitle1">Tuesday</Typography>
             </Grid>
             <Grid item xs={12}>
-              <Typography variant="subtitle1">Tuesday 5:00 PM</Typography>
+              <Typography variant="subtitle1">Sunny</Typography>
             </Grid>
           </Grid>
-          <Grid item container justify="space-between" direction="row">
-            <Grid item>current weather</Grid>
-            <Grid item>More weather info</Grid>
+          <Grid item container direction="row">
+            <Grid
+              item
+              container
+              justify="flex-start"
+              direction="row"
+              alignItems="center"
+              spacing={1}
+              xs={6}
+            >
+              <Grid item>
+                <CardMedia
+                  image={sunny}
+                  style={{
+                    height: "60px",
+                    width: "60px",
+                    alignSelf: "center",
+                    justify: "center",
+                  }}
+                />
+              </Grid>
+
+              <Grid item>
+                <Typography variant="h2">22</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>°C</Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={6}>
+              More weather info
+            </Grid>
           </Grid>
         </Grid>
         <Grid item style={{ margin: "auto" }}>
--- weather-app/src/components/WeatherCard.js
+++ weather-app/src/components/WeatherCard.js
@@ -10,16 +10,6 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 // Weather icons
-import sunny from "../images/sunny.png";
-import cloud from "../images/cloud.png";
-import cloudy from "../images/cloudy.png";
-import lightning from "../images/lightning.png";
-import mooncloud from "../images/mooncloud.png";
-import moon from "../images/moon.png";
-import rain from "../images/rain.png";
-import rainbow from "../images/rainbow.png";
-import snow from "../images/snow.png";
-import sun from "../images/sun.png";
 
 const useStyles = makeStyles((theme) => ({
   weatherCard: {
@@ -40,40 +30,6 @@ const useStyles = makeStyles((theme) => ({
 export default function WeatherCard(props) {
   const classes = useStyles();
   const { date, weather, maxTemp, minTemp } = props;
-  let weatherIcon;
-
-  switch (weather) {
-    case 0:
-      weatherIcon = sunny;
-      break;
-    case 1:
-      weatherIcon = cloud;
-      break;
-    case 2:
-      weatherIcon = cloudy;
-      break;
-    case 3:
-      weatherIcon = lightning;
-      break;
-    case 4:
-      weatherIcon = moon;
-      break;
-    case 5:
-      weatherIcon = mooncloud;
-      break;
-    case 6:
-      weatherIcon = rain;
-      break;
-    case 7:
-      weatherIcon = rainbow;
-      break;
-    case 8:
-      weatherIcon = snow;
-      break;
-    default:
-      weatherIcon = sun;
-      break;
-  }
 
   return (
     <Box className={classes.weatherCard} color="#000000">
@@ -87,7 +43,7 @@ export default function WeatherCard(props) {
           style={{ textAlign: "center" }}
         />
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <CardMedia className={classes.weatherIcon} image={weatherIcon} />
+          <CardMedia className={classes.weatherIcon} image={weather} />
         </div>
         <CardContent>
           <Grid container spacing={2} justify="center">
