b1d145ee95bf9e4805a0cfd7b54625c589ac49dc
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-10-04 23:28:58 +0900

    Temporary name for the app

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ export default function App() {
   return <React.Fragment>
     <div style={{backgroundColor: "#fafafa"}}>
     <AppBar style={{margin: 'auto', height: 80, backgroundColor: '#EC6E4C', justifyContent: "center", alignItems: "center"}}>
-        <Typography variant="h6">
-          COVID statistics
+        <Typography variant="h4">
+          InfoCards
         </Typography>
     </AppBar>
     <Container style={{marginTop: 80, padding: 25}}>
